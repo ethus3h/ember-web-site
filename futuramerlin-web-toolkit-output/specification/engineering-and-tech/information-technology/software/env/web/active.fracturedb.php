efaa4fe82bfae882465433de58d87ff10cce1ea8
ethus3h <kolubat@gmail.com>
2019-04-23 17:34:49 -0400

    Update Git repository using egup-single

--- futuramerlin-web-toolkit-output/specification/engineering-and-tech/information-technology/software/env/web/active.fracturedb.php
+++ futuramerlin-web-toolkit-output/specification/engineering-and-tech/information-technology/software/env/web/active.fracturedb.php
@@ -255,7 +255,7 @@ class FractureDB
     {
         $query    = 'SELECT * FROM ' . $table . ' WHERE ' . $field . ' = \'' . $value . '\';';
         $rowData  = $this->query($query);
-        //print_r($rowData);
+        print_r($rowData);
         if(isset($rowData[0])) {
         	$rowDataP = $rowData[0];
         }
@@ -436,11 +436,11 @@ class FractureDB
     }
     function addRowFromArrays($table, $fields, $values, $print="false")
     {
-    if($print=="true"){
-    print_r($fields);
-    print_r($values);
-    }
-		$dbh      = $this->db;
+        if($print=="true"){
+            print_r($fields);
+            print_r($values);
+        }
+        $dbh      = $this->db;
         $i=0;
         $n=count($values);
         $placeholders="";
