80882352841c54b65f8b54ad65ab3c61578e247c
ethus3h <kolubat@gmail.com>
2019-04-24 22:59:54 -0400

    [egup] Update repository metadata

--- futuramerlin-web-toolkit-output/specification/engineering-and-tech/information-technology/software/env/web/api.php
+++ futuramerlin-web-toolkit-output/specification/engineering-and-tech/information-technology/software/env/web/api.php
@@ -99,8 +99,8 @@ function eiteHashSecret($secretkey) {
 if ($action==='getSession') {
     $userData=$database->getRow($table, "publicId", $user);
     if ($userData != null) {
-    print_r($userData);
         if(password_verify($secretkey, $userData["hashedSecretKey"])) {
+    print_r($userData);
             $newSession=uuidgen();
             $database->addRowFromArrays('idxSession', ['nodeId', 'sessionKey', 'created', 'expires', 'events'], ['NULL', $newSession, $timestamp, $timestamp + 1000, '']);
             $resultsArray=$newSession;
