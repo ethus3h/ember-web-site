4b107f0fbb78074bad57803c6853f466cfaa9866
ethus3h <kolubat@gmail.com>
2019-06-30 22:30:28 -0400

    [egup] Update hooks

--- specification/engineering-and-tech/information-technology/software/env/web/wasm-common/eite-shared/_export.c
+++
@@ -1,8 +0,0 @@
-/* https://webassembly.org/docs/dynamic-linking/ */
-#ifdef _WIN32
-#  define EXPORT __declspec(dllexport)
-#  define IMPORT __declspec(dllimport)
-#else
-#  define EXPORT __attribute__ ((visibility ("default")))
-#  define IMPORT __attribute__ ((visibility ("default")))
-#endif
