df8600bd6a113be46e312b0c816dbf68c8d3868e
ethus3h <kolubat@gmail.com>
2020-01-20 21:25:44 -0500

    Update Git repository using egup-single

---
+++ built/people/elli/writings/blogs/newmedia/2019-Spring-NMD445/2019m02d18b-JSON.js
@@ -0,0 +1,16 @@
+{
+    "event1Id": {
+        "description": "Hey, party 6PM here :)",
+        "time": "10:00 PM"
+    },
+    "event2Id": {
+        "description": "The production server is down!",
+        "time": "9:12 PM",
+        "priority": "urgent"
+    },
+    "event3Id": {
+        "description": "Flood watch: Orono",
+        "time": "Yesterday",
+        "priority": "emergency"
+    }
+}
